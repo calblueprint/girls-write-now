f1208d2210a8a969e9dc1ad3c3d53aad8a4962dd
Akshay Thakur <akshaythakur@berkeley.edu>
2023-09-21 17:23:38 -0700

    fix npm start scripts issue in package.json

--- App.tsx
+++ App.tsx
@@ -4,7 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>The Girls Write Now Team!</Text>
+      <Text>Akshay</Text>
+      <Text>Brenda</Text>
+      <Text>...</Text>
+      <Text>...</Text>
+      <Text>...</Text>
+      <Text>...</Text>
       <StatusBar style="auto" />
     </View>
   );

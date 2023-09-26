57937cc886c5fa6721ceb130f4a7349646a684f2
Akshay Thakur <akshaythakur@berkeley.edu>
2023-09-25 19:56:38 -0700

    testing status check

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>The Girls Write Now Team!</Text>
     </View>
   );
 }

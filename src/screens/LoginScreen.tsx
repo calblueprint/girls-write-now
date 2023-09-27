4dfaba692df88b1f16ae9f13206a3f766c100bee
Aditya Pawar <aditya.pawar@warriorlife.net>
2023-09-26 20:32:26 -0700

    Add components from tutorials

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import { useState, useEffect } from 'react';
+import { supabase } from '../lib/supabase';
+import Login from '../components/Login';
+import Account from '../components/Account';
+import { View } from 'react-native';
+import { Session } from '@supabase/supabase-js';
 
-export default function LoginScreen() {
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Page</Text>
+    <View>
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <Login />
+      )}
     </View>
   );
 }

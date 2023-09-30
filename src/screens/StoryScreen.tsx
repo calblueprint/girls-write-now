fbd4c945ea4301547fc28de0b9a8ee99f8e6aef7
Marcos Hernandez <marcosh@berkeley.com>
2023-09-30 01:44:54 -0700

    npx prettier . --write

--- src/screens/StoryScreen.tsx
+++ src/screens/StoryScreen.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+
+// type Story = {
+//   content: string;
+// };
 
 export default function StoryScreen() {
+  const [isLoading, setLoading] = useState(true);
+  const [title, setTitle] = useState(String);
+  const [content, setContent] = useState(String);
+
+  const getStory = async () => {
+    try {
+      const response = await fetch(
+        'https://girlswritenow.org/wp-json/wp/v2/story/170947',
+      );
+      const json = await response.json();
+      setTitle(json.title.rendered);
+      setContent(json.content.rendered);
+    } catch (error) {
+      // console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getStory();
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Story Page</Text>
+
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text>
+          {title}
+          {'\n'}
+          {content}
+        </Text> // title shows weird symbols and content shows
+      )}
     </View>
   );
 }

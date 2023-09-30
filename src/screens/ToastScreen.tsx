1de1ce0b9b4ef101c395b03ae910e7498440adfa
Inaya Yusuf <inayayusuf.29@gmail.com>
2023-09-29 21:04:56 -0700

    trying to commit after adding toast dependencies

--- src/screens/ToastScreen.tsx
+++ src/screens/ToastScreen.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+// import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { RootSiblingParent } from 'react-native-root-siblings';
+import Toast from 'react-native-root-toast';
 
 export default function ToastScreen() {
+  
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Toast Page</Text>
-    </View>
+      <RootSiblingParent>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Toast Page</Text>
+          <Button 
+            onPress={() => Toast.show} 
+            title="Toast"
+            /* color = 'blue' /*messing around */
+          />
+        </View>
+    </RootSiblingParent>
   );
 }
+
+const toast = Toast.show('Testing this out?', {
+  duration: Toast.durations.LONG,
+  position: Toast.positions.BOTTOM,
+  shadow: true,
+  animation: true,
+  hideOnPress: true,
+  delay: 0,
+  // onShow: () => {},
+  // onShown: () => {},
+  // onHide: () => {},
+  // onHidden: () => {}
+  });

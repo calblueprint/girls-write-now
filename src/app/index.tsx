e2eee6d08ae557aceaba72a681104ad6fe028ba6
Akshay Thakur <akshaythakur@berkeley.edu>
2023-10-02 15:28:08 -0700

    trying out expo routes

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'expo-router';
+import React from 'react';
+import { Button, View } from 'react-native';
+
+export default function WelcomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ padding: 20 }}>
+            <Link href="/home" asChild>
+                <Button title="HOME PAGE"/>
+            </Link>
+        </View>
+    </View>
+  );
+}

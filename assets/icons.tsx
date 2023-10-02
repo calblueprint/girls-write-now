f1bfd9a9b379bc59de371b345c89dbc500686998
Akshay Thakur <akshaythakur@berkeley.edu>
2023-10-02 16:45:51 -0700

    update nav

--- assets/icons.tsx
+++ assets/icons.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 export type IconType =
   | 'home_outline'
+  | 'search_outline'
   | 'document_outline'
   | 'notifications_outline';
 
 const IconSvgs: Record<IconType, React.ReactElement> = {
   home_outline: <Ionicons name="home-outline" size={23} />,
+  search_outline: <Ionicons name="search-outline" size={23} />,
   document_outline: <Ionicons name="document-outline" size={23} />,
   notifications_outline: <Ionicons name="notifications-outline" size={23} />,
 };

a7fea68b48e8a8dd64dc35f4598aad37deea42c5
Marcos Hernandez <marcosh@berkeley.com>
2023-10-06 02:28:51 -0700

    getStory function now abstractly takes any valid story id to display

--- src/screens/StoryScreen.tsx
+++ src/screens/StoryScreen.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, ScrollView, View } from 'react-native';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import HTMLView from 'react-native-htmlview';
 
 export default function StoryScreen() {
@@ -8,23 +7,22 @@ export default function StoryScreen() {
   const [title, setTitle] = useState(String);
   const [content, setContent] = useState(String);
 
-  const getStory = async () => {
+  const getStory = async (id: string) => {
     try {
-      const response = await fetch(
-        'https://girlswritenow.org/wp-json/wp/v2/story/170947',
-      );
+      const url = `https://girlswritenow.org/wp-json/wp/v2/story/${id}`;
+      const response = await fetch(url);
       const json = await response.json();
       setTitle(json.title.rendered);
       setContent(json.content.rendered);
     } catch (error) {
-      // console.error(error);
+      console.error(error);
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    getStory();
+    getStory('170947');
   }, []);
 
   return (

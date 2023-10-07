e30c9365a240a3a87468fd49121699aa86bea928
Aditya Pawar <34043950+adityapawar1@users.noreply.github.com>
2023-10-07 12:35:43 -0700

    [auth] Implement email account creation (#10)
    * Add components from tutorials
    
    * Add profile columns to update page
    
    * Update supabase data working
    
    * Only update non-blank entries
    
    * Run eslint fix
    
    * Fix eslint errors
    
    * Fix tsc errors
    
    * Fix trivial review changes
    
    * Add components from tutorials
    
    * Add profile columns to update page
    
    * Update supabase data working
    
    * Only update non-blank entries
    
    * Run eslint fix
    
    * Fix eslint errors
    
    * Fix tsc errors
    
    * Fix trivial review changes
    
    * Downgrade ts to be compatible with deps
    
    * Add components from tutorials
    
    * Add profile columns to update page
    
    * Update supabase data working
    
    * Only update non-blank entries
    
    * Run eslint fix
    
    * Fix eslint errors
    
    * Fix trivial review changes
    
    * Add components from tutorials
    
    * Add profile columns to update page
    
    * Update supabase data working
    
    * Only update non-blank entries
    
    * Run eslint fix
    
    * Fix eslint errors
    
    * Fixes after merge :(
    
    * Fix eslint errors
    
    * Fix routing for sign up/in
    
    * Downgrade ts
    
    * Fix andriod date taking up the screen
    
    * Fix build
    
    * Downgrade ts
    
    * Fix android date issue
    
    * Fix package-lock.jsonO
    
    ---------
    
    Co-authored-by: Aditya Pawar <aditya.pawar@warriorlife.net>

---
+++ src/components/Login.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+import { Button, Input } from 'react-native-elements';
+import supabase from '../utils/supabase';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: 'stretch',
+  },
+  mt20: {
+    marginTop: 20,
+  },
+});
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const signInWithEmail = async () => {
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+  };
+
+  const signUpWithEmail = async () => {
+    setLoading(true);
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) Alert.alert(error.message);
+    console.error(error);
+    setLoading(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Input
+          label="Email"
+          leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+          onChangeText={text => setEmail(text)}
+          value={email}
+          placeholder="email@address.com"
+          autoCapitalize="none"
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="Password"
+          leftIcon={{ type: 'font-awesome', name: 'lock' }}
+          onChangeText={text => setPassword(text)}
+          value={password}
+          secureTextEntry
+          placeholder="Password"
+          autoCapitalize="none"
+        />
+      </View>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Button title="Sign in" disabled={loading} onPress={signInWithEmail} />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Button title="Sign up" disabled={loading} onPress={signUpWithEmail} />
+      </View>
+    </View>
+  );
+}

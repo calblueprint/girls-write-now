24efe19c34100bcf54ffdc50edc3ad9330f87efe
Marcos Hernandez <marcosh@berkeley.com>
2023-10-10 20:29:19 -0700

    moved html parser function to global section of story file

--- src/app/(tabs)/search/story.tsx
+++ src/app/(tabs)/search/story.tsx
@@ -4,6 +4,13 @@ import HTMLView from 'react-native-htmlview';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import globalStyles from '../../../styles/globalStyles';
 
+function htmlParser(html: string) {
+  const regex = /<h2(.*?)h2>(\n+<p(.*?)p>)+/; // regex grabs heading and paragraph tags for story
+  const corresp = regex.exec(html);
+  const story = corresp ? corresp[0] : ''; // <h2>heading<h2> <p>paragraph1</p> ...
+  return story;
+}
+
 function StoryScreen() {
   const [isLoading, setLoading] = useState(true);
   const [title, setTitle] = useState(String);
@@ -21,13 +28,6 @@ function StoryScreen() {
     } finally {
       setLoading(false);
     }
-  };
-
-  const htmlParser = (html: string) => {
-    const regex = /<h2(.*?)h2>(\n+<p(.*?)p>)+/; // regex grabs heading and paragraph tags for story
-    const corresp = regex.exec(html);
-    const story = corresp ? corresp[0] : ''; // <h2>heading<h2> <p>paragraph1</p> ...
-    return story;
   };
 
   useEffect(() => {

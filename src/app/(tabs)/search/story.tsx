6fc0279eba210b609345244f75804636b4303a9f
Marcos Hernandez <marcosh@berkeley.com>
2023-10-19 22:08:14 -0700

    finished back to top button

--- src/app/(tabs)/search/story.tsx
+++ src/app/(tabs)/search/story.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ActivityIndicator,
+  FlatList,
   Image,
   ScrollView,
   Share,
   Text,
   View,
 } from 'react-native';
-import HTMLView from 'react-native-htmlview';
 import { Button } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
 import jsonStory from '../../../database/story.json';
 import globalStyles from '../../../styles/globalStyles';
-import temp from '../../../styles/temp';
 
 function htmlParser(htmlString: string) {
   const regexHeading = /(<h2(.*?)h2>)/;
@@ -24,10 +24,26 @@ function htmlParser(htmlString: string) {
   const process = regexProcess.exec(htmlString);
 
   const contentHeading = heading
-    ? heading[0].replace('<h2', '<h3').replace('</h2>', '</h3>')
-    : ''; // <h2>heading<h2>
-  const contentStory = story ? story[0].replace(/(\n)+/gi, '') : ''; // <p>paragraph1</p> ...
-  const contentProcess = process ? process[0].replace('&nbsp;', '') : '';
+    ? heading[0]
+        .replace('</h2>', '')
+        .replace(/<h2.+>/, '')
+        .replace(/&#8217;/gi, "'")
+    : '';
+  const contentStory = story
+    ? story[0]
+        .replace(/(\n)+/gi, '')
+        .replace(/<p>/gi, '')
+        .replace(/<\/p>/gi, '\n\n')
+        .replace(/&nbsp;/gi, '')
+        .replace(/&#8217;/gi, "'")
+    : '';
+  const contentProcess = process
+    ? process[0]
+        .replace(/<p>/gi, '')
+        .replace(/<\/p>/gi, '')
+        .replace(/&nbsp;/gi, '')
+        .replace(/&#8217;/gi, "'")
+    : '';
   return {
     heading: contentHeading,
     story: contentStory,
@@ -38,10 +54,13 @@ function htmlParser(htmlString: string) {
 function StoryScreen() {
   const [isLoading, setLoading] = useState(true);
   const [title, setTitle] = useState(String);
-  const [content, setContent] = useState(String);
+  const [story, setStory] = useState(String);
+  const [heading, setHeading] = useState(String);
+  const [process, setProcess] = useState(String);
   const [author, setAuthor] = useState(String);
-  const [genres, setGenres] = useState(String);
+  const [genres, setGenres] = useState(['']);
   const [image, setImage] = useState(String);
+  const ref = React.useRef<any>(null);
 
   // Load Wordpress API and its contents
   const getStory = async (id: string) => {
@@ -50,15 +69,12 @@ function StoryScreen() {
       const response = await fetch(url);
       const json = await response.json();
 
-      setTitle(json.title.rendered);
-      setContent(json.content.rendered);
+      setTitle(json.title.rendered.replace(/&#8217;/gi, "'"));
+      setStory(htmlParser(json.content.rendered).story);
+      setHeading(htmlParser(json.content.rendered).heading);
+      setProcess(htmlParser(json.content.rendered).process);
       setAuthor(jsonStory.author);
-      setGenres(
-        `<body><ul>${jsonStory['genre-medium']
-          .map(txt => `<li>${txt}</li>`)
-          .toString()
-          .replace(',', '')}</ul></body>`,
-      );
+      setGenres(jsonStory['genre-medium']);
       setImage(jsonStory.featured_media);
     } catch (error) {
       console.error(error);
@@ -74,10 +90,10 @@ function StoryScreen() {
         message:
           `${title}\n` +
           `By ${author}\n\n\n` +
-          `${htmlParser(content).heading.replace(/<[^>]+>/g, '\n')}` +
-          `${htmlParser(content).story.replace(/<[^>]+>/g, '\n')}\n\n\n` +
+          `${heading}` +
+          `${story}\n` +
           `Author's Process:\n` +
-          `${htmlParser(content).process.replace(/<[^>]+>/g, '')}`,
+          `${process}`,
       });
       if (result.action === Share.sharedAction) {
         if (result.activityType) {
@@ -93,6 +109,10 @@ function StoryScreen() {
     }
   };
 
+  const scrollUp = () => {
+    ref.current?.scrollTo({ x: 0, y: 0 });
+  };
+
   useEffect(() => {
     getStory('170947');
   }, []);
@@ -102,87 +122,85 @@ function StoryScreen() {
       {isLoading ? (
         <ActivityIndicator />
       ) : (
-        <ScrollView>
-          {/* STYLING & URI WARNING */}
-          <Image
-            style={temp.image}
-            source={{ uri: image ? image : undefined }}
-          />
+        <ScrollView bounces ref={ref}>
+          <Image style={tempStyles.image} source={{ uri: image }} />
 
-          <View>
-            <HTMLView
-              value={title.replace(title, `<body><br><h2>${title}</h2></body>`)}
-            />
+          <View style={tempStyles.top}>
+            <Text style={tempStyles.title}>{title}</Text>
+
+            <View style={tempStyles.author}>
+              <View
+                style={{
+                  width: 21,
+                  height: 21,
+                  borderRadius: 100 / 2,
+                  backgroundColor: '#D9D9D9',
+                }}
+              />
+              <Text style={tempStyles.authorText}>By {author}</Text>
+            </View>
+
+            <View style={{ alignItems: 'flex-start' }}>
+              <FlatList
+                style={tempStyles.genres}
+                horizontal
+                data={genres}
+                renderItem={({ item }) => (
+                  <Text style={tempStyles.genresText}>{item}</Text>
+                )}
+              />
+            </View>
+
+            <View style={{ alignItems: 'flex-start' }}>
+              <Button
+                textColor="black"
+                buttonColor="#D9D9D9"
+                icon="share"
+                onPress={onShare}
+              >
+                <Text style={tempStyles.shareButtonText}>Share Story</Text>
+              </Button>
+            </View>
           </View>
 
-          <View style={{ display: 'flex', flexDirection: 'row' }}>
-            <Button textColor="black" icon="circle">
-              {''}
-            </Button>
-            <Text style={temp.body}>By {author}</Text>
-            <Text>{'\n'}</Text>
+          <Text style={tempStyles.heading}>{heading}</Text>
+
+          <Text style={tempStyles.story}>{story}</Text>
+
+          <View style={tempStyles.bottom}>
+            <View style={{ alignItems: 'flex-start' }}>
+              <Button
+                textColor="black"
+                buttonColor="#D9D9D9"
+                icon="share"
+                onPress={onShare}
+              >
+                <Text style={tempStyles.shareButtonText}>Share Story</Text>
+              </Button>
+            </View>
+
+            <Text style={globalStyles.h4}>Author's Process</Text>
+
+            <Text style={tempStyles.process}>{process}</Text>
+
+            <View style={tempStyles.author}>
+              <View
+                style={{
+                  width: 21,
+                  height: 21,
+                  borderRadius: 100 / 2,
+                  backgroundColor: '#D9D9D9',
+                }}
+              />
+              <Text style={tempStyles.authorText}>By {author}</Text>
+            </View>
+
+            <View style={{ alignItems: 'flex-start' }}>
+              <Button textColor="black" icon="arrow-up" onPress={scrollUp}>
+                <Text style={tempStyles.backToTopButtonText}>Back To Top</Text>
+              </Button>
+            </View>
           </View>
-
-          <View style={{ backgroundColor: '#D9D9D9', borderRadius: 10 }}>
-            <HTMLView value={genres} />
-          </View>
-          <Text>{'\n'}</Text>
-
-          <View style={{ alignItems: 'flex-start' }}>
-            <Button
-              textColor="black"
-              buttonColor="#D9D9D9"
-              icon="share"
-              onPress={onShare}
-            >
-              Share Story
-            </Button>
-          </View>
-          <Text>{'\n'}</Text>
-
-          <HTMLView value={htmlParser(content).heading} />
-
-          <HTMLView value={htmlParser(content).story} />
-          <Text>{'\n'}</Text>
-
-          <View style={{ alignItems: 'flex-start' }}>
-            <Button
-              textColor="black"
-              buttonColor="#D9D9D9"
-              icon="share"
-              onPress={onShare}
-            >
-              Share Story
-            </Button>
-          </View>
-          <Text>{'\n'}</Text>
-
-          <Text style={globalStyles.h4}>Author's Process</Text>
-          <Text>{'\n'}</Text>
-
-          <HTMLView value={htmlParser(content).process} />
-          <Text>{'\n'}</Text>
-
-          <View style={{ display: 'flex', flexDirection: 'row' }}>
-            <Button textColor="black" icon="circle">
-              {''}
-            </Button>
-            <Text style={temp.body}>By {author}</Text>
-          </View>
-          <Text>{'\n\n'}</Text>
-
-          {/* <Button onPress={backToTop} title="Back to Top" /> */}
-          <View style={{ alignItems: 'flex-start' }}>
-            <Button
-              textColor="black"
-              buttonColor="#D9D9D9"
-              icon="arrow-up"
-              onPress={onShare}
-            >
-              Back To Top
-            </Button>
-          </View>
-          <Text>{'\n\n\n\n\n\n\n\n\n\n'}</Text>
         </ScrollView>
       )}
     </SafeAreaView>
@@ -190,3 +208,94 @@ function StoryScreen() {
 }
 
 export default StoryScreen;
+
+const tempStyles = {
+  image: {
+    width: '100%',
+    height: '5%',
+  },
+  top: {
+    justifyContent: 'space-between',
+    gap: 16,
+    paddingTop: 20,
+    paddingBottom: 34,
+  },
+  bottom: {
+    justifyContent: 'space-between',
+    gap: 16,
+    paddingBottom: 160,
+  },
+  title: {
+    fontFamily: 'Avenir',
+    fontSize: 24,
+    fontWeight: '400',
+    textAlign: 'left',
+    color: 'black',
+  },
+  author: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+  },
+  authorText: {
+    fontFamily: 'Avenir',
+    fontSize: 12,
+    fontWeight: '400',
+    textAlign: 'left',
+    color: 'black',
+  },
+  genres: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    borderRadius: 10,
+  },
+  genresText: {
+    fontFamily: 'Avenir',
+    fontSize: 12,
+    fontWeight: '400',
+    color: 'black',
+    backgroundColor: '#D9D9D9',
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  shareButtonText: {
+    fontFamily: 'Avenir',
+    fontSize: 12,
+    fontWeight: '400',
+    textAlign: 'left',
+    color: 'black',
+    textDecorationLine: 'underline',
+  },
+  heading: {
+    fontFamily: 'Avenir',
+    fontSize: 16,
+    fontWeight: '400',
+    textAlign: 'left',
+    color: 'black',
+    paddingBottom: 34,
+  },
+  story: {
+    fontFamily: 'Avenir',
+    fontSize: 12,
+    fontWeight: '400',
+    textAlign: 'left',
+    color: 'black',
+  },
+  process: {
+    fontFamily: 'Avenir',
+    fontSize: 12,
+    fontWeight: '400',
+    textAlign: 'left',
+    color: 'black',
+  },
+  backToTopButtonText: {
+    fontFamily: 'Avenir',
+    fontSize: 12,
+    fontWeight: '600',
+    textAlign: 'left',
+    color: 'black',
+  },
+};

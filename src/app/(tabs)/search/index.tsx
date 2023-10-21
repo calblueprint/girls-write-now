332b1df7950a55d9612461e3929feb3543983ac6
Marcos Hernandez <marcosh@berkeley.com>
2023-10-21 16:59:52 -0700

    began working on search bar. No functionality

--- src/app/(tabs)/search/index.tsx
+++ src/app/(tabs)/search/index.tsx
@@ -1,15 +1,64 @@
+import { SearchBar } from '@rneui/themed';
 import { Link } from 'expo-router';
-import { Button, Text, View } from 'react-native';
+import React, { SetStateAction, useState } from 'react';
+import { Button, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import globalStyles from '../../../styles/globalStyles';
 
 function SearchScreen() {
+  const [search, setSearch] = useState('');
+  const [allStories, setAllStories] = useState<any>([]);
+  const [searchResults, setSearchResults] = useState<any>([]);
+
+  const updateSearch = (search: string) => {
+    setSearch(search);
+  };
+
+  const searchFunction = (text: string) => {
+    if (text === '') {
+      setSearchResults(allStories);
+    }
+    // We want to filter our full list of stories so that it contains only those
+    // that match our input text.
+    const updatedData = allStories.filter((item: any) => {
+      // Make the story's title uppercase
+      const title = `${item.title.toUpperCase()})`;
+      // Make the story's author uppercase
+      const author = `${item.author.toUpperCase()})`;
+      // Make the input text uppercase
+      const text_data = text.toUpperCase();
+      // This basically means that if the title or the author contains the
+      // input text, we want to include this story in updatedData
+      return title.indexOf(text_data) > -1 || author.indexOf(text_data) > -1;
+    });
+    setSearchResults(updatedData);
+  };
+
   return (
-    <SafeAreaView style={globalStyles.container}>
-      <View>
-        <Text style={globalStyles.h1}>Welcome, name</Text>
-      </View>
+    <SafeAreaView>
+      <SearchBar
+        platform="default"
+        searchIcon
+        clearIcon
+        containerStyle={{}}
+        inputContainerStyle={{}}
+        inputStyle={{}}
+        leftIconContainerStyle={{}}
+        rightIconContainerStyle={{}}
+        lightTheme
+        loadingProps={{}}
+        onChangeText={newVal => setSearch(newVal)}
+        placeholder="Type query here..."
+        placeholderTextColor="#888"
+        value={search}
+      />
+      <ScrollView>
+        <Text>placeholder</Text>
+        {/* <SearchCard />
+        <SearchCard />
+        ... */}
+      </ScrollView>
       <Link href="/search/story" asChild>
         <Button title="Story" />
       </Link>

5f187de79d0cac9e36578944d326d9ba14a6f9aa
Akshay Thakur <78326649+akshaynthakur@users.noreply.github.com>
2023-10-31 14:50:08 -0700

    [search] Implement call to Supabase for story search results (#29)

---
+++ src/queries/types.tsx
@@ -0,0 +1,11 @@
+export interface StoryPreview {
+  id: number;
+  date: string;
+  title: string;
+  featured_media: string;
+  author_name: string;
+  author_image: string;
+  topic: string[];
+  tone: string[];
+  genre_medium: string[];
+}

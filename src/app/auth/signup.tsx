be0b3c8af75e9eae661bca37207c932f9c0c13a3
Aditya Pawar <aditya.pawar@warriorlife.net>
2023-10-31 20:23:46 -0700

    We do a lil bit of styling

--- src/app/auth/signup.tsx
+++ src/app/auth/signup.tsx
@@ -1,14 +1,18 @@
 import { Redirect, Link, router } from 'expo-router';
-import React, { useState } from 'react';
-import { Alert, View } from 'react-native';
-import { Button, Input } from 'react-native-elements';
+import React, { useId, useState } from 'react';
+import { Alert, Text, View, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
 
 import globalStyles from '../../styles/globalStyles';
 import { useSession } from '../../utils/AuthContext';
+import { TextInput } from 'react-native-paper';
 
 function SignUpScreen() {
   const { session, signUp } = useSession();
 
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
@@ -29,39 +33,76 @@ function SignUpScreen() {
 
   return (
     <View style={globalStyles.auth_container}>
+      <Text style={[globalStyles.h3, globalStyles.mt20]}>
+        Read stories from young creators
+      </Text>
       <View style={[globalStyles.verticallySpaced, globalStyles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-          onChangeText={text => setEmail(text)}
-          value={email}
-          placeholder="email@address.com"
+        <TextInput
+          onChangeText={text => setUsername(text)}
+          value={username}
+          style={styles.inputField}
+          placeholder="Enter New Username"
+          autoCapitalize="none"
+        />
+      </View>
+      <View style={[globalStyles.verticallySpaced, globalStyles.mt20]}>
+        <TextInput
+          onChangeText={text => setFirstName(text)}
+          value={firstName}
+          style={styles.inputField}
+          placeholder="First Name"
+          autoCapitalize="none"
+        />
+      </View>
+      <View style={[globalStyles.verticallySpaced, globalStyles.mt20]}>
+        <TextInput
+          onChangeText={text => setLastName(text)}
+          value={lastName}
+          style={styles.inputField}
+          placeholder="Last Name"
           autoCapitalize="none"
         />
       </View>
 
-      <View style={globalStyles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: 'font-awesome', name: 'lock' }}
+      <View style={[globalStyles.verticallySpaced, globalStyles.mt20]}>
+        <TextInput
+          onChangeText={text => setEmail(text)}
+          value={email}
+          style={styles.inputField}
+          placeholder="Enter Email"
+          autoCapitalize="none"
+          textContentType="emailAddress"
+        />
+      </View>
+      <View style={[globalStyles.verticallySpaced, globalStyles.mt20]}>
+        <TextInput
           onChangeText={text => setPassword(text)}
           value={password}
-          secureTextEntry
-          placeholder="Password"
+          style={styles.inputField}
+          placeholder="Enter Password"
           autoCapitalize="none"
+          secureTextEntry={true}
+          textContentType={'password'}
         />
+      </View>
 
-        <Link href="/auth/login">Already have an account? Log In</Link>
-        <View style={[globalStyles.verticallySpaced, globalStyles.mt20]}>
-          <Button
-            title="Sign Up"
-            disabled={loading}
-            onPress={signUpWithEmail}
-          />
-        </View>
+      <Link href="/auth/login">Already have an account? Log In</Link>
+      <View style={[globalStyles.verticallySpaced, globalStyles.mt20]}>
+        <Button title="Sign Up" disabled={loading} onPress={signUpWithEmail} />
       </View>
     </View>
   );
 }
 
 export default SignUpScreen;
+
+const styles = StyleSheet.create({
+  inputField: {
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: 'transparent',
+  },
+  button: {
+    backgroundColor: 'gray',
+  },
+});

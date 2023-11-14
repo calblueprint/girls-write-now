302da3cda79dcced470583bb2395d02e78d35009
Akshay Thakur <akshaythakur@berkeley.edu>
2023-11-13 21:33:55 -0800

    pr review changes

--- src/app/(tabs)/home/index.tsx
+++ src/app/(tabs)/home/index.tsx
@@ -30,23 +30,28 @@ function HomeScreen() {
 
   useEffect(() => {
     (async () => {
-      const usernameResponse = await fetchUsername(user?.id);
+      const [
+        usernameResponse,
+        featuredStoryResponse,
+        featuredStoryDescriptionResponse,
+        recommendedStoriesResponse,
+        newStoriesResponse,
+      ] = await Promise.all([
+        fetchUsername(user?.id).catch(() => ''),
+        fetchFeaturedStoryPreviews().catch(() => []),
+        fetchFeaturedStoriesDescription().catch(() => ''),
+        fetchRecommendedStories().catch(() => []),
+        fetchNewStories().catch(() => []),
+      ]);
       setUsername(usernameResponse);
-      const featuredStoryResponse: StoryPreview[] =
-        await fetchFeaturedStoryPreviews();
       setFeaturedStories(featuredStoryResponse);
-      const featuredStoryDescriptionResponse: string =
-        await fetchFeaturedStoriesDescription();
       setFeaturedStoriesDescription(featuredStoryDescriptionResponse);
-      const recommendedStoriesResponse: StoryCard[] =
-        await fetchRecommendedStories();
       setRecommendedStories(recommendedStoriesResponse);
-      const newStoriesResponse: StoryCard[] = await fetchNewStories();
       setNewStories(newStoriesResponse);
     })().finally(() => {
       setLoading(false);
     });
-  }, []);
+  }, [user]);
   return (
     <SafeAreaView style={[globalStyles.container, { marginHorizontal: -8 }]}>
       {loading && (
@@ -61,67 +66,85 @@ function HomeScreen() {
         contentContainerStyle={{ paddingHorizontal: 8 }}
       >
         <View style={styles.headerContainer}>
-          <Text style={globalStyles.h2}>Welcome, {username}</Text>
+          <Text style={globalStyles.h2}>
+            {username ? `Welcome, ${username}` : 'Welcome!'}
+          </Text>
           <Pressable onPress={() => router.push('/settings')}>
             <View>
               <Icon type="settings_gear" />
             </View>
           </Pressable>
         </View>
-        <Text style={globalStyles.h3}>Featured Stories</Text>
-        <Text style={[globalStyles.body1, styles.featuredDescription]}>
-          {featuredStoriesDescription}
-        </Text>
-        <View>
-          {featuredStories.map(story => (
-            <PreviewCard
-              key={story.title}
-              title={story.title}
-              image={story.featured_media}
-              author={story.author_name}
-              authorImage={story.author_image}
-              excerpt={story.excerpt}
-              tags={story.genre_medium.concat(story.tone).concat(story.topic)}
-              pressFunction={() => null}
-            />
-          ))}
-        </View>
-        <Text style={[globalStyles.h3, styles.subheading]}>
-          Recommended For You
-        </Text>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          bounces={false}
-          style={styles.scrollView}
-        >
-          {recommendedStories.map(story => (
-            <ContentCard
-              key={story.title}
-              title={story.title}
-              author={story.author_name}
-              pressFunction={() => null}
-              image={story.featured_media}
-            />
-          ))}
-        </ScrollView>
-        <Text style={[globalStyles.h3, styles.subheading]}>New Stories</Text>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          bounces={false}
-          style={styles.scrollView}
-        >
-          {newStories.map(story => (
-            <ContentCard
-              key={story.title}
-              title={story.title}
-              author={story.author_name}
-              pressFunction={() => null}
-              image={story.featured_media}
-            />
-          ))}
-        </ScrollView>
+        {featuredStories.length > 0 && (
+          <View>
+            <Text style={globalStyles.h3}>Featured Stories</Text>
+            <Text style={[globalStyles.body1, styles.featuredDescription]}>
+              {featuredStoriesDescription}
+            </Text>
+            <View>
+              {featuredStories.map(story => (
+                <PreviewCard
+                  key={story.title}
+                  title={story.title}
+                  image={story.featured_media}
+                  author={story.author_name}
+                  authorImage={story.author_image}
+                  excerpt={story.excerpt}
+                  tags={story.genre_medium
+                    .concat(story.tone)
+                    .concat(story.topic)}
+                  pressFunction={() => null}
+                />
+              ))}
+            </View>
+          </View>
+        )}
+        {recommendedStories.length > 0 && (
+          <View>
+            <Text style={[globalStyles.h3, styles.subheading]}>
+              Recommended For You
+            </Text>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              bounces={false}
+              style={styles.scrollView}
+            >
+              {recommendedStories.map(story => (
+                <ContentCard
+                  key={story.title}
+                  title={story.title}
+                  author={story.author_name}
+                  pressFunction={() => null}
+                  image={story.featured_media}
+                />
+              ))}
+            </ScrollView>
+          </View>
+        )}
+        {newStories.length > 0 && (
+          <View>
+            <Text style={[globalStyles.h3, styles.subheading]}>
+              New Stories
+            </Text>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              bounces={false}
+              style={styles.scrollView}
+            >
+              {newStories.map(story => (
+                <ContentCard
+                  key={story.title}
+                  title={story.title}
+                  author={story.author_name}
+                  pressFunction={() => null}
+                  image={story.featured_media}
+                />
+              ))}
+            </ScrollView>
+          </View>
+        )}
       </ScrollView>
     </SafeAreaView>
   );

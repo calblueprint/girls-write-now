6b4e80d12532771b28dd9ab5fe3f0aff862c8b03
Emily Sunaryo <eadorasunaryo@berkeley.edu>
2023-11-28 19:45:26 -0800

    fixing rebase issues

--- src/queries/types.tsx
+++ src/queries/types.tsx
@@ -47,61 +47,6 @@ export interface StoryCard {
   author_name: string;
   featured_media: string;
 }
-export interface RecentSearch {
-  value: string;
-  numResults: number;
-}
-export interface Story {
-  id: number;
-  date: string;
-  title: string;
-  featured_media: string;
-  author_id: number;
-  author_name: string;
-  author_image: string;
-  topic: string[];
-  tone: string[];
-  genre_medium: string[];
-  excerpt: { html: string };
-  content: { html: string };
-  process: { html: string };
-  link: string;
-}
-
-export interface StoryCard {
-  id: number;
-  title: string;
-  author_name: string;
-  featured_media: string;
-}
-export interface RecentSearch {
-  value: string;
-  numResults: number;
-}
-
-export interface Story {
-  id: number;
-  date: string;
-  title: string;
-  featured_media: string;
-  author_id: number;
-  author_name: string;
-  author_image: string;
-  topic: string[];
-  tone: string[];
-  genre_medium: string[];
-  excerpt: { html: string };
-  content: { html: string };
-  process: { html: string };
-  link: string;
-}
-
-export interface StoryCard {
-  id: number;
-  title: string;
-  author_name: string;
-  featured_media: string;
-}
 
 export interface Subgenre {
   id: number;

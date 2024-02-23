913ba9586da455dea1adf646ad8358cca19fba30
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2024-02-22 21:31:26 -0800

    prettier

---
+++ src/app/auth/resetPassword/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+
+function StackLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
+
+export default StackLayout;
+//fefefr

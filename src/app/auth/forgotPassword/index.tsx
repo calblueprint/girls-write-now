7fa58ef95a8ac2991728075107d8d306a4c88748
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2024-02-29 22:09:48 -0800

    implemented username checks and displaying error messages, routing to signUp

--- src/app/auth/forgotPassword/index.tsx
+++ src/app/auth/forgotPassword/index.tsx
@@ -9,44 +9,53 @@ import { useSession } from '../../../utils/AuthContext';
 import UserStringInput from '../../../components/UserStringInput/UserStringInput';
 import StyledButton from '../../../components/StyledButton/StyledButton';
 import { isEmailTaken } from '../../../queries/profiles';
+import { queryEmailByUsername } from '../../../queries/auth';
 
 function ForgotPasswordScreen() {
   const { updateUser, signOut, resetPassword, verifyOtp } = useSession();
   const [email, setEmail] = useState('');
+  const [emailToReset, setEmailToReset] = useState('');
   const [emailError, setEmailError] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
   const [validEmail, setValidEmail] = useState(false);
 
   const sendResetEmail = async () => {
-    const { error } = await resetPassword(email);
+    const { error } = await resetPassword(emailToReset);
     if (error)
       Alert.alert('Could not send a reset password email. Please try again.');
-    else
-      Alert.alert(
-        'Enter the verification code from your email to change your password',
-      );
+    else router.replace('auth/signup');
   };
 
-  const setAndCheckEmail = async (newEmail: string) => {
+  const setAndCheckEmailOrUsername = async (newEmail: string) => {
     setEmail(newEmail);
-    if (newEmail.length === 0) {
-      setEmailError('');
-      return;
-    }
+    if (validator.isEmail(newEmail)) {
+      if (newEmail.length === 0) {
+        setEmailError('');
+        return;
+      }
+      const emailIsTaken = await isEmailTaken(newEmail);
+      if (!emailIsTaken) {
+        setEmailError(
+          'An account with that email does not exist. Please try again.',
+        );
+        setValidEmail(false);
+        return;
+      }
+      setEmailToReset(newEmail);
+    } else {
+      const { data, error } = await queryEmailByUsername(newEmail);
 
-    if (!validator.isEmail(newEmail)) {
-      setEmailError('This email is not a valid email. Please try again.');
-      return;
+      if (data && data?.length > 0 && !error) {
+        setValidEmail(true);
+        setEmailToReset(data[0].email);
+      } else {
+        setEmailError(
+          'An account with that username does not exist. Please try again.',
+        );
+        setValidEmail(false);
+        return;
+      }
     }
-
-    const emailIsTaken = await isEmailTaken(newEmail);
-    if (emailIsTaken) {
-      setEmailError(
-        'An account with that email does not exist. Please try again.',
-      );
-      return;
-    }
+    setValidEmail(true);
     setEmailError('');
   };
 
@@ -59,12 +68,17 @@ function ForgotPasswordScreen() {
           placeholderTextColor="#797979"
           value={email}
           label="Email or account username"
-          onChange={setAndCheckEmail}
+          onChange={setAndCheckEmailOrUsername}
         />
         <Text style={[globalStyles.errorMessage, styles.subtext]}>
           We'll email you a code to confirm your email.
         </Text>
+
+        {email !== '' && (
+          <Text style={[globalStyles.errorMessage]}>{emailError}</Text>
+        )}
       </View>
+
       <View>
         <StyledButton
           disabled={!validEmail}

f597dc4213e49a9c71d6cecb26e8ec363a16a8bb
Emily Sunaryo <eadorasunaryo@berkeley.edu>
2024-02-29 23:42:03 -0800

    ran prettier

--- src/app/(tabs)/genre/styles.tsx
+++ src/app/(tabs)/genre/styles.tsx
@@ -1,3 +1,39 @@
 import { StyleSheet } from 'react-native';
 
 import colors from '../../../styles/colors';
+import globalStyles from '../../../styles/globalStyles';
+
+const styles = StyleSheet.create({
+  textSelected: {
+    color: '#EB563B',
+    textDecorationLine: 'underline',
+  },
+
+  container: {
+    width: '100%',
+    marginTop: 24,
+    flex: 1,
+  },
+
+  flatListStyle: {
+    paddingTop: 15,
+  },
+  scrollViewContainer: {
+    marginTop: 30,
+    marginBottom: 15,
+    width: '100%',
+  },
+  noStoriesText: {
+    fontSize: 20,
+    color: '#EB563B',
+  },
+  noStoriesText2: {
+    fontSize: 13,
+  },
+  renderStories: {
+    flex: 1,
+  },
+  headerContainer: {},
+});
+
+export default styles;

0bbe80d5c56fd2eceb57c5b412011d2b9773b5c4
Marcos Hernandez <marcosh@berkeley.com>
2024-03-03 12:46:56 -0800

    categories created

--- src/queries/savedStories.tsx
+++ src/queries/savedStories.tsx
@@ -1,4 +1,3 @@
-import { Story } from './types';
 import supabase from '../utils/supabase';
 
 export async function fetchUserStories(
@@ -22,9 +21,41 @@ export async function fetchUserStories(
 }
 
 export async function fetchUserStoriesFavorites(user_id: string | undefined) {
-  return fetchUserStories(user_id, 'favorites');
+  fetchUserStories(user_id, 'favorites');
 }
 
 export async function fetchUserStoriesReadingList(user_id: string | undefined) {
-  return fetchUserStories(user_id, 'reading_list');
+  fetchUserStories(user_id, 'reading list');
+}
+
+export async function setUserStories(
+  user_id: string | undefined,
+  story_id: number,
+  name: string,
+) {
+  const { data, error } = await supabase
+    .from('Saved Stories')
+    .insert([{ user_id: user_id, story_id: story_id, name: name }])
+    .select();
+
+  if (error) {
+    console.log(error);
+    throw new Error(
+      `An error occured when trying to set user saved stories: ${error.details}`,
+    );
+  }
+}
+
+export async function setUserStoriesFavorites(
+  user_id: string | undefined,
+  story_id: number,
+) {
+  setUserStories(user_id, story_id, 'favorites');
+}
+
+export async function setUserStoriesReadingList(
+  user_id: string | undefined,
+  story_id: number,
+) {
+  setUserStories(user_id, story_id, 'reading list');
 }

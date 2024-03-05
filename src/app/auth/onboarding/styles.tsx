ce6f13a7e7c9d447cb0454185d63b1d3fcceb39f
Justin Lee <91563628+jjstnlee@users.noreply.github.com>
2024-03-04 18:37:32 -0800

    [auth] Update onboarding page to have demographic collection warning (#56)
    * Added demographic warning
    
    * Random
    
    * Updated onboarding page to have demographic collection warning
    
    * Updated onboarding page to have demographic collection warning
    
    * Fixed requested changes
    
    * Fixed requested styling changes
    
    * Changed margin spacing to match Figma
    
    ---------
    
    Co-authored-by: Justin Lee <jjunseol04@berkeley.edu>

--- src/app/auth/onboarding/styles.tsx
+++ src/app/auth/onboarding/styles.tsx
@@ -1,5 +1,7 @@
 import { StyleSheet } from 'react-native';
 
+import colors from '../../../styles/colors';
+
 export default StyleSheet.create({
   input: {
     height: 40,
@@ -17,5 +19,22 @@ export default StyleSheet.create({
     paddingVertical: 63,
     paddingLeft: 43,
     paddingRight: 44,
+  },
+  subtext: {
+    color: colors.darkGrey,
+    marginLeft: 8,
+  },
+  h1: {
+    marginTop: 66,
+  },
+  body1: {
+    marginTop: 26,
+  },
+  info: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 12,
+    marginBottom: 16,
+    width: 250,
   },
 });
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+
 import colors from './colors';
 
 export default StyleSheet.create({

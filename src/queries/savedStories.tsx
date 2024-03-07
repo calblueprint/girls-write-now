2dbf1cf3b68894017c87ef3c7c67c144a219fddb
Marcos Hernandez <marcosh@berkeley.com>
2024-03-06 22:52:27 -0800

    favorites and reading list global variables set

--- src/queries/savedStories.tsx
+++ src/queries/savedStories.tsx
@@ -1,11 +1,14 @@
 import supabase from '../utils/supabase';
 
+const favorites = 'favorites';
+const readingList = 'reading list';
+
 export async function fetchUserStories(
   user_id: string | undefined,
   name: string | undefined,
 ) {
   const { data, error } = await supabase
-    .from('Saved Stories')
+    .from('saved_stories')
     .select('story_id')
     .eq('user_id', user_id)
     .eq('name', name);
@@ -21,11 +24,11 @@ export async function fetchUserStories(
 }
 
 export async function fetchUserStoriesFavorites(user_id: string | undefined) {
-  fetchUserStories(user_id, 'favorites');
+  fetchUserStories(user_id, favorites);
 }
 
 export async function fetchUserStoriesReadingList(user_id: string | undefined) {
-  fetchUserStories(user_id, 'reading list');
+  fetchUserStories(user_id, readingList);
 }
 
 export async function setUserStories(
@@ -34,7 +37,7 @@ export async function setUserStories(
   name: string,
 ) {
   const { data, error } = await supabase
-    .from('Saved Stories')
+    .from('saved_stories')
     .insert([{ user_id: user_id, story_id: story_id, name: name }])
     .select();
 
@@ -50,14 +53,14 @@ export async function setUserStoriesFavorites(
   user_id: string | undefined,
   story_id: number,
 ) {
-  setUserStories(user_id, story_id, 'favorites');
+  setUserStories(user_id, story_id, favorites);
 }
 
 export async function setUserStoriesReadingList(
   user_id: string | undefined,
   story_id: number,
 ) {
-  setUserStories(user_id, story_id, 'reading list');
+  setUserStories(user_id, story_id, readingList);
 }
 
 export async function deleteUserStories(
@@ -66,7 +69,7 @@ export async function deleteUserStories(
   name: string,
 ) {
   const { error } = await supabase
-    .from('Saved Stories')
+    .from('saved_stories')
     .delete()
     .eq('user_id', user_id)
     .eq('story_id', story_id)

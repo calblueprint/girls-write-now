0250b5959587932edf14c66c9c8719f75112312d
Justin Lee <91563628+jjstnlee@users.noreply.github.com>
2024-03-14 21:57:54 -0700

    Added ability to dismiss keyboard when tapping screen (#68)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from 'expo-router';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { AuthContextProvider } from '../utils/AuthContext';
@@ -6,17 +7,19 @@ import ToastComponent from '../components/Toast/Toast';
 
 function StackLayout() {
   return (
-    <SafeAreaProvider>
-      <AuthContextProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="settings" options={{ headerShown: false }} />
-          <Stack.Screen name="auth" options={{ headerShown: false }} />
-        </Stack>
-      </AuthContextProvider>
-      <ToastComponent />
-    </SafeAreaProvider>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <SafeAreaProvider>
+        <AuthContextProvider>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="settings" options={{ headerShown: false }} />
+            <Stack.Screen name="auth" options={{ headerShown: false }} />
+          </Stack>
+        </AuthContextProvider>
+        <ToastComponent />
+      </SafeAreaProvider>
+    </TouchableWithoutFeedback>
   );
 }
 

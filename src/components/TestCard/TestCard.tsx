451f1fcb94349e3fdff8983723c2bd093f71025f
Aditya Pawar <apawar@berkeley.edu>
2024-03-16 12:54:44 -0700

    Run prettier

--- src/components/TestCard/TestCard.tsx
+++ src/components/TestCard/TestCard.tsx
@@ -8,7 +8,12 @@ import {
 
 import styles from './styles';
 import globalStyles from '../../styles/globalStyles';
-import { addUserStoryToFavorites, addUserStoryToReadingList, fetchUserStoriesFavorites, fetchUserStoriesReadingList } from '../../queries/savedStories'
+import {
+  addUserStoryToFavorites,
+  addUserStoryToReadingList,
+  fetchUserStoriesFavorites,
+  fetchUserStoriesReadingList,
+} from '../../queries/savedStories';
 import { useState } from 'react';
 import { useSession } from '../../utils/AuthContext';
 import { Button } from 'react-native-paper';
@@ -18,20 +23,30 @@ function TestCard() {
   const [storyId, setStoryId] = useState('');
 
   return (
-    <Pressable >
+    <Pressable>
       <View style={styles.contentCard}>
         <View style={styles.textContainer}>
-          <TextInput
-            onChangeText={t => setStoryId(t)}
-            value={storyId}
-          />
-          <Text style={[globalStyles.h3, styles.title]} numberOfLines={1} onPress={async () => await addUserStoryToReadingList(user?.id, parseInt(storyId))}>
+          <TextInput onChangeText={t => setStoryId(t)} value={storyId} />
+          <Text
+            style={[globalStyles.h3, styles.title]}
+            numberOfLines={1}
+            onPress={async () =>
+              await addUserStoryToReadingList(user?.id, parseInt(storyId))
+            }
+          >
             Add story to favorites
           </Text>
-          <Text style={globalStyles.body1} onPress={async () => await addUserStoryToFavorites(user?.id, parseInt(storyId))}>
+          <Text
+            style={globalStyles.body1}
+            onPress={async () =>
+              await addUserStoryToFavorites(user?.id, parseInt(storyId))
+            }
+          >
             Add story to reading list
           </Text>
-          <Button onPress={() => fetchUserStoriesFavorites(user?.id)}>log</Button>
+          <Button onPress={() => fetchUserStoriesFavorites(user?.id)}>
+            log
+          </Button>
         </View>
       </View>
     </Pressable>
@@ -39,4 +54,3 @@ function TestCard() {
 }
 
 export default TestCard;
-

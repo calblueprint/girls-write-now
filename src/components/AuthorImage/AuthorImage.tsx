fc546f95cfbfacc761be78dbcbfd6606cf94c9b3
Emily Sunaryo <eadorasunaryo@berkeley.edu>
2024-04-21 22:20:37 -0700

    progress on story styling

--- src/components/AuthorImage/AuthorImage.tsx
+++ src/components/AuthorImage/AuthorImage.tsx
@@ -1,23 +1,42 @@
+import { router } from 'expo-router';
 import {
   GestureResponderEvent,
   Text,
   TouchableOpacity,
   View,
+  Image,
 } from 'react-native';
 
 import styles from './styles';
 
 type AuthorImageProps = {
-  authorname: string;
-  authorUri: string;
+  author_name: string;
+  author_id: number;
+  author_Uri: string;
   pressFunction: (event: GestureResponderEvent) => void;
 };
 
-function AuthorImage({ pressFunction }: AuthorImageProps) {
+function AuthorImage({
+  author_name,
+  author_id,
+  author_Uri,
+  pressFunction,
+}: AuthorImageProps) {
   return (
-    <TouchableOpacity onPress={pressFunction}>
-      <View style={styles.authorImageContainer}>
-        <Text>testing</Text>
+    <TouchableOpacity
+      onPress={() => {
+        router.push({
+          pathname: '/author',
+          params: { author: author_id.toString() },
+        });
+      }}
+    >
+      <View style={styles.author}>
+        <Image
+          style={styles.author_image}
+          source={{ uri: author_Uri ? author_Uri : '' }}
+        />
+        <Text style={styles.authorText}>By {author_name}</Text>
       </View>
     </TouchableOpacity>
   );
